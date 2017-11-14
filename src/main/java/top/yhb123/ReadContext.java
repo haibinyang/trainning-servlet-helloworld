2841c81c0e056051d52af2f1788de5c8f72f963d
haibin.yang <haibin.yang@phicomm.com>
2017-11-14 17:20:48 +0800

    添加ServletContext

---
+++ src/main/java/top/yhb123/ReadContext.java
@@ -0,0 +1,35 @@
+package top.yhb123;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ReadContext extends HttpServlet {
+    private String message;
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("[HB]init");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //设置响应内容类型
+        resp.setContentType("text/html");
+        resp.setContentType("utf-8");
+
+        //设置逻辑实现
+        PrintWriter out = resp.getWriter();
+
+        String appConfig = getServletContext().getInitParameter("appConfig");
+        out.println("Context param = " + appConfig);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
