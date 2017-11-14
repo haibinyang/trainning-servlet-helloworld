9eddac9cbfabeecedcdc3209b540177e99659cce
haibin.yang <haibin.yang@phicomm.com>
2017-11-14 15:56:19 +0800

    v2

--- src/main/java/top/yhb123/BeerSelect.java
+++ src/main/java/top/yhb123/BeerSelect.java
@@ -1,5 +1,7 @@
 package top.yhb123;
 
+import top.yhb123.model.BeerExpert;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
@@ -14,7 +16,14 @@ public class BeerSelect extends HttpServlet {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		out.println("Beer Selection Advice<br>");
+
 		String color = request.getParameter("color");
-		out.println("Got color: " + color);
+		BeerExpert be = new BeerExpert();
+		List result = be.getBrands(color);
+
+		Iterator it = result.iterator();
+		while (it.hasNext()) {
+			out.print("<br/>try: " + it.next());
+		}
 	}		
 }
